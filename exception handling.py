cb88486cb25dbb7ff5fba611d0a6be5fb246a790
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-07-31 19:45:28 +0200

    Add files via upload

---
+++ exception handling.py
@@ -0,0 +1,32 @@
+# exception handling
+
+try:
+    age = int(input('Enter Age:'))
+    print(100/age)
+    # processing x
+
+    # close
+
+
+except ValueError:
+    print('please enter number and number > 0')
+    # close
+
+
+except ZeroDivisionError:
+    print('please enter number and number > 0')
+    # close
+
+
+else: # no excptions
+    print('in else')
+
+
+
+
+finally: # always
+    print('closing ....')
+
+
+print('procesing')
+
