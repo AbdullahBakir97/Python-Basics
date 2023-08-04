f976acd92321adb695fb058624ac22c8a99d2c5e
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-08-04 18:33:32 +0200

    Update exception handling.py

--- exception handling.py
+++ exception handling.py
@@ -1,16 +1,21 @@
 # exception handling
 
 # if-else 
+import math
+
 age = input('Enter Age')
 if age.replace('.',',').isnumeric():
     if float(age) > 0:
         print(round(100/float(age)),2)
+        print(math.floor(100/float(age)))
+        print(math.ceil(100/float(age)))
     else:
         print('please enter number > 0')
 else:
     print('please enter number')
 
 print('procesing')
+
 #############################################
 #try-except 
 try:
